4760fb8e2e586e3bef406921e4315f9362aa4bbd
Andrew213455 <woodring.andrew@yahoo.com>
2023-06-23 15:10:45 -0400

    styling, timer functionality, notes

--- functions/src/models/Account.ts
+++ functions/src/models/Account.ts
@@ -1,10 +1,12 @@
 import { ObjectId } from "mongodb";
 import Recipe from "./Recipe";
+import Notes from "./Notes";
 
 export default interface Account {
   _id: ObjectId;
   googleId?: string;
   name: string;
   favorites: Recipe[];
+  notes: Notes[];
   photoURL: string;
 }
